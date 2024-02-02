a1f309a54d08d2bda2709d1dac777312f1b2a5f4
DILLIP KUMAR PUTI <putikishore63@gmail.com>
2024-02-02 19:15:26 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cityInput, setCityInput] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const apiKey = "92e74ee899264aa79a5142100242901";
+
+  const searchWeather = () => {
+    setLoading(true);
+    setError("");
+
+    fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+
+        if (data.error) {
+          alert("Failed to fetch weather data.");
+        } else {
+          setWeatherData(data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+        setError("Failed to fetch weather data. Please try again.");
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background"></div>
+      <div className="content">
+        <h1>Weather App</h1>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={cityInput}
+          onChange={(e) => setCityInput(e.target.value)}
+        />
+        <button onClick={searchWeather}>Search</button>
+        {loading && <p>Loading data...</p>}
+        {error && <p className="error">{error}</p>}
+        {weatherData && (
+          <div id="weather-container">
+            <div className="weather-box">
+              <p>
+                <strong>Temperature</strong> <br /> {weatherData.current.temp_c}
+                °C
+              </p>
+            </div>
+            <div className="weather-box">
+              <p>
+                <strong>Humidity</strong> <br /> {weatherData.current.humidity}%
+              </p>
+            </div>
+            <div className="weather-box">
+              <p>
+                <strong>Condition</strong> <br />
+                {weatherData.current.condition.text}
+              </p>
+            </div>
+            <div className="weather-box">
+              <p>
+                <strong>Wind Speed</strong> <br />
+                {weatherData.current.wind_kph} km/h
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
